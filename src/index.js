a0ca0fdfb997d3757352e8701f6107eff502936c
Ramu Arumugam <ramu.8215@gmail.com>
2025-11-14 13:30:53 +0800

    frontend ui project code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <h1>Frontend is working!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
